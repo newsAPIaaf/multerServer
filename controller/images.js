2c04298aced7130096d3ed3a61a21380430df47a
idabagusangga <angga.idabagus@gmail.com>
2018-02-14 18:05:24 +0700

    multer done

--- controller/images.js
+++ controller/images.js
@@ -1,7 +1,7 @@
 class ImageController {
   static postPicture (req, res) {
     // console.log(req.body, '--------------ini req body');
-    // console.log(req.file, '--------------ini req file');
+    console.log(req.file, '--------------ini req file');
     res.status(200).json({
       ...req.file
     })
