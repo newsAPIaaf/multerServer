d95a57147003b3f435acf333f8e642d0551c28b8
aldiws <syldie.aldiwijaya@gmail.com>
2018-02-15 10:37:55 +0700

    walmart-api

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors')
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var images = require('./routes/image')
+var walmart = require('./routes/walmart')
 
 
 var app = express();
@@ -19,6 +19,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(cors())
 app.use(bodyParser.json());
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/images', images)
+app.use('/walmart', walmart)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
